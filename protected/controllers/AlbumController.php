055855a816862c06877ce251f2e2b318998bd387
ageesh <ageeshkg@gmail.com>
2012-08-16 04:35:13 -0700

    album page added

---
+++ protected/controllers/AlbumController.php
@@ -0,0 +1,29 @@
+<?php
+/*
+*
+* $Id$
+--------------------------------------------------------------------------------------------------------------------------
+* Information contained in this file is the intellectual property of Ladbrokes Plc
+* Copyright © 2011 MarryDoor. All Rights Reserved.
+* ---------------------------------------------------------------------------------------------------------------------------
+*
+* @author  Dileep G.
+* @title AlbumController.php
+* @description <Description of this class>
+*  @filesource <URL>
+*  @version <Revision>
+*/
+class AlbumController extends Controller
+{
+	
+	/**
+	 * This is the default 'index' action that is invoked
+	 * when an action is not explicitly requested by users.
+	 */
+	public function actionIndex()
+	{
+
+		$this->render('index');
+	}
+	
+}
---
+++ protected/views/album/index.php
@@ -0,0 +1,55 @@
+    	<div id="album_main"><!--content-wrapper-->
+			<!--wrapper-head-->
+			<div class="name-bloc"><!--name-bloc-->
+				<br />
+				<p class="text_pink-hd">Lilly Mathews MD123456</p> 
+				<div class="clear"></div>
+				<p class="txt_rg">Viewing album</p>
+			</div><!--name-bloc-->
+			
+			<div class="img-bloc"><!--img-bloc-->
+			<a href="#">	<img src="<?php echo Yii::app()->params['resourceUrl']; ?>/images/pop-up/11-album-document-1.jpg" class="gal-img_22" border="0"/></a>
+			<a href="#">	<img src="<?php echo Yii::app()->params['resourceUrl']; ?>/images/pop-up/11-album-document-2.jpg" class="gal-img_22" border="0"/></a>
+			<a href="#">	<img src="<?php echo Yii::app()->params['resourceUrl']; ?>/images/pop-up/11-album-document-3.jpg" class="gal-img_22" border="0"/></a>
+			<a href="#">	<img src="<?php echo Yii::app()->params['resourceUrl']; ?>/images/pop-up/11-album-document-2.jpg" class="gal-img_22" border="0"/></a>
+			<a href="#">	<img src="<?php echo Yii::app()->params['resourceUrl']; ?>/images/pop-up/11-album-document-5.jpg" class="gal-img_22" border="0"/></a>
+			</div><!--img-bloc-->
+			
+		  <div id="big-wrapper"><!--big-wrapper-->
+			
+				<div class="content-top"><!--content-top-->
+				
+							<a class="exp-sub-less" href="#">Express Interest</a>
+
+   <a class="exp-sub-add-less" href="#">Bookmark</a>
+
+     <a class="exp-sub-send-less" href="#">Send Message</a>
+
+			  </div><!--content-top-->
+              
+              
+				
+			<div class="star-img"><!--star-img-->
+		      <a href="#" class="star-panne-44"/> </a>
+              
+			<img src="<?php echo Yii::app()->params['resourceUrl']; ?>/images/pop-up/model.jpg" class="left" />
+			   
+            <a href="#" class="star-pannel-right-55"/></a>
+            
+			</div><!--star-img-->
+		<div class="clear"></div>
+        
+        	
+			<div class="content-bottom"><!--content-bottom-->
+				
+			
+            <a class="exp-sub-less" href="#">Express Interest</a>
+
+   <a class="exp-sub-add-less" href="#">Bookmark</a>
+
+     <a class="exp-sub-send-less" href="#">Send Message</a>
+
+            </div><!--big-wrapper-->
+        	
+            
+          </div>
