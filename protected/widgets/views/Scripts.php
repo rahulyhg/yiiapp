0f78e86cf29b75fcd1cb67c6a676debb67830bea
ageesh <ageeshkg@gmail.com>
2012-12-11 21:21:39 +0530

    added validations

--- protected/widgets/views/Scripts.php
+++ protected/widgets/views/Scripts.php
@@ -1,9 +1,11 @@
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->params['resourceUrl']; ?>/css/marrydoor.css" />
     <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->params['resourceUrl']; ?>/css/global.css" />
-    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->params['resourceUrl']; ?>/css/colorbox.css" />
-    <script src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/jquery.js"></script>
+    <link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->params['resourceUrl']; ?>/css/validationEngine.jquery.css"/>
+    <script src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/jquery1.7.2.min.js"></script>
     <script src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/modernizr-1.7.min.js"></script>
 	<script src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/inr.js"></script>
 	<script src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/marrydoor.js"></script>
-	<script src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/jquery.colorbox-min.js"></script>
+	<script type="text/javascript" src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/jquery.validationEngine.js" charset="utf-8"></script>
+	<script type="text/javascript" src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/jquery.validationEngine-en.js" charset="utf-8"></script>
+	<script type="text/javascript" src="<?php echo Yii::app()->params['resourceUrl']; ?>/js/formvalidation.js" charset="utf-8"></script>
 	
