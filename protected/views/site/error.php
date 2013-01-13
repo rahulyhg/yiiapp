7dcffa73f9624802f52b39591ea59fad4029fb07
ageesh <ageeshkg@gmail.com>
2013-01-13 23:48:18 +0530

    404 error page upload

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -5,8 +5,12 @@ $this->breadcrumbs=array(
 );
 ?>
 
-<h2>Error <?php echo $code; ?></h2>
-
-<div class="error">
-<?php echo CHtml::encode($message); ?>
-</div>
+ <section class="page404">
+		<div class="md404"></div>
+		<div class="pnf-data">
+			<div class="pnf-head">404!</div>
+			<div class="pnf-subhead">Ouch, We<br />Think You Hit<br />A Dead End.</div>
+			<div class="pnf-text">The page you are looking for<br />something that isn't here. <br />But fret not; this event has <br /> been logged and we'll be <br />looking into this very soon.</div>
+			<a href="mailto:team@marrydoor.com" class="pnf-link">Team marrydoor.com</a>
+		</div>
+	</section>
