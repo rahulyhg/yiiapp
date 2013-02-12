2c482bc00a3e647eb3d053b48a71ec476d55f128
ageesh <ageeshkg@gmail.com>
2013-02-12 10:56:39 +0530

    terms section

---
+++ protected/views/guest/terms.php
@@ -0,0 +1,154 @@
+<?php
+/*
+*
+* $Id$
+--------------------------------------------------------------------------------------------------------------------------
+* Information contained in this file is the intellectual property of Ladbrokes Plc
+* Copyright © 2012 MarryDorr. All Rights Reserved.
+* ---------------------------------------------------------------------------------------------------------------------------
+*
+* @author  Ageesh K Gopinath
+* @title terms.php
+* @description <Description of this class>
+*  @filesource <URL>
+*  @version <Revision>
+*/
+?>
+
+		<div class="subContent">
+			<section class="subHead">
+				<h1>Terms and Conditions </h1>
+			</section>
+			<section class="subContnr">
+				<ul class="accOverview mTB20">
+					<li>
+						<p>
+							Dear User,
+						</p>
+						<p>
+							Welcome to Marrydoor (hereinafter termed as "MD"). The use of this website or access of any of the services provided on the website is subject to your acceptance of these terms and conditions. So request you to read these Terms and Conditions carefully. All rights, privileges, obligations and liabilities of you and/or MD with respect to any transactions or services by, with or in connection with MD for all purposes shall be governed by this agreement. The terms and conditions may be changed and/or altered by MD from time to time at its sole discretion.
+						</p>
+						<p>
+							1. <strong>Usage of the website</strong>
+						</p>
+						<p>
+							Your use of this website or access of any of the services provided on the website is subject to your acceptance of these terms and conditions. Therefore, please read these carefully. If you require any clarification about these terms and conditions, please contact <a href="mailto:support@marrydoor.com"> support@marrydoor.com </a> and obtain such clarification before using this Website.
+						</p>
+						<p>
+							Your use of the website means your agreement in full to all terms and conditions, policies and notices contained herein. If you do not accept any of these terms and conditions, policies or notices contained on the website, please do not use or access the website.
+						</p>
+						<p>
+							For the purposes of this Website, "User" shall mean any person who uses the Website, and/or any of the services provided on the Website including but not limited to any person who posts any content including but not limited to text, data, images, video, information, and other materials or services on the Website or who views such content or materials. The Website shall be deemed to be used by you when it is loaded and/or continues to be loaded in temporary or permanent memory of your mobile phone, computer or any other device or in the internet browser thereof.
+						</p>
+						<p>
+							MD reserves the absolute discretion to amend, delete or modify any of these terms and conditions at any time, which amendment, deletion or modification as the case may be shall automatically become binding on the User when the amendment, deletion or supplement is made on the Website.
+						</p>
+						<p>
+							The use of this Website by you may be discontinued, terminated or limited at any time by  MD without any prior notice to you if you violate any part of the terms and conditions or for any reason whatsoever.
+						</p>
+						<p>
+							2. <strong>Eligibility</strong>
+						</p>
+						<p>
+							MD is intended to facilitate lawful marriages between persons by providing a medium for contact between such persons. It is therefore essential that users are competent of entering into a legal and binding marriage contract as per the law applicable to them. By using this Website, therefore you confirm that you are registering for a person who is: 18 years or above (for a woman) or 21 years or above (for a man) and is legally competent to enter into a valid contract of marriage as per the law applicable to you and is not prohibited or prevented by any applicable law for the time being in force from entering into a valid contract of marriage.
+						</p>
+						<p>
+							MD reserves the right to terminate your use or registration with us without refund of any of subscription fee paid, if at any time MD in its sole discretion, of the opinion or has any reason to believe that, you are not eligible to use this website or that you have made any misrepresentation about your eligibility.
+						</p>
+						<p>
+							3. <strong>Getting Registered </strong>
+						</p>
+						<p>
+							To use some of the services on MD such as expressing interest in a member or of premium services offered by the website users must be registered with MD. The initial step is to fill up the registration form available at www.marrydoor/register. During registration you may be required to furnish personal or other details. Ensure that you give in accurate, complete and current information.
+						</p>
+						<p>
+							Once registration is successful, you will be given a user name and password. By applying for registration you confirm and agree that you will be the sole and exclusive owner and that you accept sole responsibility for the use, confidentiality and protection of the user name and the password as well as for all actions initiated using your user name and password. Your registration with MD is for your personal use. You may not authorize others to use your user name and password and you may not assign or transfer your user name or password to any other person or entity. All statements made and acts or omissions that occur while your user name and password are being used shall be deemed to have originated from your end and you shall be held responsible for the same or for any misuse thereof.
+						</p>
+						<p>
+							Multiple registrations of the same person are not allowed. MD reserves the right to deactivate all multiple registrations by the same user without any refund of subscription fees.
+						</p>
+						<p>
+							You are required to immediately notify MD in writing if you come to know of any loss or unauthorized use of your user name or password or any inaccurate information in relation to your use of the Website or our services. While MD disclaims any liability for such loss, or unauthorised use and consequences thereof, on receipt of such notification from you, MD will take steps as reasonably possible to block your user name and on a request duly made, generate a new user name and password for you.
+						</p>
+						<p>
+							If any information provided by you for registration or otherwise is found to be untrue, inaccurate, not current or incomplete, or MD has reasons to suspect that such information is untrue, inaccurate, not current or incomplete, MD has the right to suspend or terminate your registration with the website and refuse you any and all current or future use of the website.
+						</p>
+						<p>
+							MD reserves the right to reject you registration on the website, terminate your registration and delete or edit any content posted on the website using your user name or password.
+						</p>
+						<p>
+							4. <strong>Content</strong>
+						</p>
+						<p>
+							You are responsible for the content and information including your profile, photograph and other details including communication with other Users you post or transmit through MD. You hereby represent and warrant that you own all rights, title and interest in your details or have the necessary licenses, rights, consents, and permissions to post or use the said content.
+						</p>
+						<p>
+							You will retain all your ownership rights in content you post on the Website. However, by submitting the content to the website, you hereby grant MD a worldwide, non-exclusive, royalty-free, sub-licensable and transferable license to use, reproduce, distribute, prepare derivative works of, display, publish, communicate to the public and perform the content on the website. While MD does not accept any obligation to monitor the content, MD reserves the right to review and / or verify the authenticity, accuracy and quality of content posted by you on the Website and to determine whether the said content is in compliance with these Terms and Conditions. If MD is not able to verify the content or any part thereof to its satisfaction or if the content is found not to be in compliance with the terms and conditions MD may delete the Content and / or terminate your registration with the Website without a refund of any subscription paid by you. 
+						</p>
+						<p>
+							MD does not guarantee the accuracy, integrity or quality of any content. Users wishing to use the content posted by third parties on the website are advised to verify it independently. You shall be solely responsible for your content and for all consequences arising due to or out of use of the Website and the posting of your Content on the Website including but not limited to all consequences that may arise due to third parties viewing/accessing/downloading your content and/or arising due to the website hosting/publishing/communicating your content to the public. Under no circumstances will MD be liable in any way for any content, including, but not limited to, any errors or omissions in any content, or any loss or damage of any kind incurred as a result of the use of any content made available through the Website or as a result thereof.  If a user fail to login for 4 months, the account will be deactivated and after 6 months of the account will be deleted. MD reserves the right to delete the content from the website with or without prior notice. A user can keep the account in deactivation mode for 2 years. 
+						</p>
+						<p>
+							The user has to inform MD that he/she has married another user of the website and he/she wishes to update the website with a success story. MD reserves the right to request for such documents as proof of a marriage as it may deem fit from the user or both the users. MD also reserves the right to publish the success story at its discretion.
+						</p>
+						<p>
+							6. <strong>Being an MD user</strong>
+						</p>
+						<p>
+							The right of admission of any user to the website is completely on MD. Membership and rights of admission is reserved solely for - Indian Citizens, People of Indian Origin, Non Resident Indians (NRI) and people of Indian Descent or Indian Heritage. A foreigner who is willing to marry an Indian can also be a member of MD.
+						</p>
+						<p>
+							5. <strong>Any user can continue to be a member of MD till:</strong>
+						</p>
+						<p>
+							MD discharges its obligations to you by rendering its services to you; or either MD or you terminate the membership arrangement. Once your membership with us expires, you will not be able to access your personalized messages sent, and phone numbers viewed by you. We request you to save all your contacts you had contacted already and also recharge your membership with us to enjoy access to these features. Your Membership is only for personal use. It is not to be assigned, transferred or licensed so as to be used by any other person/entity, without the written consent of MD.
+						</p>
+						<p>
+							Users are asked to make adequate enquiries before acting upon any matrimonial ad. MD does not guarantee for assurance or subscribe to the claims and representations made by advertisers regarding particulars of status, age, income of other members. 
+						</p>
+						<p>
+							8. <strong>Disclaimer</strong>
+						</p>
+						<p>
+							The website and all other services provided therein are provided on an "as is" and "as available" basis. MD disclaims all warranties of any kind, whether express or implied or statutory, including, but not limited to the implied warranties of fitness for a particular purpose, data accuracy and completeness, and all warranties relating to viruses and non-infringement in the website or any services provided by the website. MD makes no representations or warranties, either express or implied that the use of the website will not infringe any patent, copyright or other right of any third party. MD makes no warranty that the website will meet user's requirements, or that the website will be uninterrupted, timely, secure, or error free. MD does not endorse, support, verify, represent or guarantee the truthfulness, accuracy, or reliability of any information or communications posted on the website or endorse any opinions expressed by users. User hereby acknowledges that any reliance on material posted by the company or other users will be at user's own risk.
+						</p>
+						<p>
+							The exchange of user profiles or communication through or using the website is not a matrimonial offer and/or recommendation by MD. MD shall not be responsible for any loss or damage to any individual arising out of, or subsequent to, any relations established between users pursuant to the use of the website.
+						</p>
+						<p>
+							9. <strong>Limitation of liability</strong>
+						</p>
+						<p>
+							User agrees that in no event shall MD be liable for any direct, indirect, incidental, special, punitive, consequential or exemplary damages, (including, but not limited to, damages caused by personal injury, loss of reputation, loss of profits, use, data or other intangible property, business interruption, loss of privacy, loss of user content, or any other pecuniary or personal loss) to user or any third party whatsoever resulting from any (a) errors, mistakes, or inaccuracies on the website, (b) personal injury or property damage, of any nature whatsoever, resulting from user's access to and use of the website, (c) any unauthorized access to or use of the secure servers of the website or any and all personal information or financial information stored therein, (d) any bugs, viruses or the like, which may be transmitted to or through the website by any third party, (e) any errors or omissions in any content or for any loss or damage of any kind incurred as a result of user's use of any content posted, emailed, transmitted, or otherwise made available via the website, (f) any unavailability of the website or parts thereof or delay in any real time service or information provided by the website, or (h) error, interruption, suspension or termination of, or in, the website whether such error, interruption, suspension or termination was justified or not, negligent or intentional, inadvertent or advertent or any services provided by or through the website or any product purchased through the website, whether based on warranty, contract,  or any other legal theory, and whether or not the company is advised of the possibility of such damages. The foregoing limitation of liability shall apply to the fullest extent permitted by law.
+						</p>
+						<p>
+							10. <strong>Other Sites</strong>
+						</p>
+						<p>
+							Third party websites to which links are given on MD are not governed by these Terms and conditions and are governed by their own terms and conditions for use and privacy policy. MD does not in any manner whatsoever authenticate, endorse, verify or certify these third party websites or any contents therein. Please note that a User shall click on links to third party websites at his / her own risk and responsibility. MD disclaims any warranties, liabilities or guarantees, express or implied, which may be attributable, in any manner whatsoever, to the use of any third party website by a User through a link provided on our Website.
+						</p>
+						<p>
+							11. <strong>General</strong>
+						</p>
+						<p>
+							The operation of the Website may be interfered by numerous factors outside our control and MD shall not be liable for any such default in the operation, caused by circumstances beyond its control including but not restricted to acts of God, acts of the public, enemy, decrees or restraints by governmental authorities, internet, telecommunication, computer or utilities failure, strikes, war, riots, and other causes of such nature. In any such event, MD shall for the duration of such event be relieved of any such obligation under this Agreement as is affected by the said event.
+						</p>
+						<p>
+							MD may schedule downtime in the Website for maintenance purposes and suspend any service by posting a notice of such downtime on the Website or such other manner as appropriate. If there is any unscheduled down time in the Website during which no User can access any service, MD shall use all reasonable resources to promptly restore the Website to its proper operating condition and notify the Users as soon as reasonably practicable after the Website is available for use again. 
+						</p>
+						<p>
+							These Terms, and any rights and licenses granted hereunder, may not be transferred or assigned by User, but may be assigned by MD without restriction.
+						</p>
+						<p>
+							The Website and the User's use of the Website and the provision of services by the Website are governed solely by Indian law, without regard to its conflict of law principles. This Website is deemed to be based in PLACE Kerala, India and any posting of Contents is deemed to have occurred in Kerala, India. The User hereby consents to submit to the exclusive jurisdiction and venue of high court in Kerala, India, to the exclusion of all other courts that may otherwise have had such jurisdiction in all matters, liability and disputes arising out of or relating to the use of this Website, any Contents and the provision of service by the Website. Use of this Website is unauthorized and prohibited in any jurisdiction that does not give effect to all provisions of the Terms and conditions including without limitation, this paragraph.
+						</p>
+						<p>
+							MD makes no claim that the Website and materials (including Contents) on the Website are appropriate for any particular purpose or audience, or that they may be downloaded outside of India. Access to the Website may not be legal by certain persons or in certain countries. If User accesses the Website from outside India, User is responsible for compliance with the laws of User's jurisdiction. MD accepts no liability whatsoever, direct or indirect for non-compliance by us or the Website with the laws of any country other than that of India.
+						</p>
+						<p>
+							Please check our Privacy Policy (link to that page) to know how we protect your confidential information. 
+						</p>
+					</li>
+				</ul>
+			</section>
+		
