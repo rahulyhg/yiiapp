4cc47e5d977c629ad4b57a68f3bd58d37bc19e08
ageesh <ageeshkg@gmail.com>
2013-02-19 23:03:31 +0530

    added requests model

---
+++ protected/models/Requests.php
@@ -0,0 +1,105 @@
+<?php
+
+/**
+ * This is the model class for table "requests".
+ *
+ * The followings are the available columns in table 'requests':
+ * @property string $requestId
+ * @property string $senderId
+ * @property string $receiverId
+ * @property integer $requestType
+ * @property integer $status
+ * @property string $sendDate
+ *
+ * The followings are the available model relations:
+ * @property Users $sender
+ * @property Users $receiver
+ */
+class Requests extends CActiveRecord
+{
+	/**
+	 * Returns the static model of the specified AR class.
+	 * @param string $className active record class name.
+	 * @return Requests the static model class
+	 */
+	public static function model($className=__CLASS__)
+	{
+		return parent::model($className);
+	}
+
+	/**
+	 * @return string the associated database table name
+	 */
+	public function tableName()
+	{
+		return 'requests';
+	}
+
+	/**
+	 * @return array validation rules for model attributes.
+	 */
+	public function rules()
+	{
+		// NOTE: you should only define rules for those attributes that
+		// will receive user inputs.
+		return array(
+			array('requestType, status', 'numerical', 'integerOnly'=>true),
+			array('senderId, receiverId', 'length', 'max'=>20),
+			array('sendDate', 'safe'),
+			// The following rule is used by search().
+			// Please remove those attributes that should not be searched.
+			array('requestId, senderId, receiverId, requestType, status, sendDate', 'safe', 'on'=>'search'),
+		);
+	}
+
+	/**
+	 * @return array relational rules.
+	 */
+	public function relations()
+	{
+		// NOTE: you may need to adjust the relation name and the related
+		// class name for the relations automatically generated below.
+		return array(
+			'sender' => array(self::BELONGS_TO, 'Users', 'senderId'),
+			'receiver' => array(self::BELONGS_TO, 'Users', 'receiverId'),
+		);
+	}
+
+	/**
+	 * @return array customized attribute labels (name=>label)
+	 */
+	public function attributeLabels()
+	{
+		return array(
+			'requestId' => 'Request',
+			'senderId' => 'Sender',
+			'receiverId' => 'Receiver',
+			'requestType' => 'Request Type',
+			'status' => 'Status',
+			'sendDate' => 'Send Date',
+		);
+	}
+
+	/**
+	 * Retrieves a list of models based on the current search/filter conditions.
+	 * @return CActiveDataProvider the data provider that can return the models based on the search/filter conditions.
+	 */
+	public function search()
+	{
+		// Warning: Please modify the following code to remove attributes that
+		// should not be searched.
+
+		$criteria=new CDbCriteria;
+
+		$criteria->compare('requestId',$this->requestId,true);
+		$criteria->compare('senderId',$this->senderId,true);
+		$criteria->compare('receiverId',$this->receiverId,true);
+		$criteria->compare('requestType',$this->requestType);
+		$criteria->compare('status',$this->status);
+		$criteria->compare('sendDate',$this->sendDate,true);
+
+		return new CActiveDataProvider($this, array(
+			'criteria'=>$criteria,
+		));
+	}
+}
